025776ef20307709ccc5501303b75053cf678bc1
김가현 <hyeonnni@naver.com>
2025-10-27 12:57:57 +0900

    feat(view): 게임 시작 및 입력 메시지를 관리하는 클래스 추가

---
+++ src/main/java/racingcar/view/GameMessage.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+public enum GameMessage {
+    ASK_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    ASK_COUNT("시도할 횟수는 몇 회인가요?"),
+    START("\n실행 결과"),
+    FINAL_WINNER("최종 우승자 : ");
+
+    private final String message;
+
+    GameMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
