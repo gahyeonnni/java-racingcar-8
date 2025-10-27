60de78147107cf8b14cddebf0a3222e507b51db6
김가현 <hyeonnni@naver.com>
2025-10-27 12:54:56 +0900

    feat(race): 자동차 이름 입력값을 검증하고 반환하는 클래스 추가

---
+++ src/main/java/racingcar/race/CarNameParser.java
@@ -0,0 +1,19 @@
+package racingcar.race;
+
+import racingcar.errorMessage.ErrorMessage;
+import java.util.regex.Pattern;
+
+public class CarNameParser {
+    private static final Pattern NAME_PATTERN = Pattern.compile("^([^,]{1,5})(,[^,]{1,5}){0,8}$");
+
+    public String[] getCarName(String input) {
+        validateNameList(input);
+        return input.split(",");
+    }
+
+    private void validateNameList(String nameList) {
+        if (!NAME_PATTERN.matcher(nameList).matches()) {
+            throw new IllegalArgumentException(ErrorMessage.NON_CORRECT_NAMES.message());
+        }
+    }
+}
