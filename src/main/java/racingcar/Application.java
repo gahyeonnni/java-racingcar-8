0d37f84683c10dcae169a3430a673c2e4cdd998d
김가현 <hyeonnni@naver.com>
2025-10-27 13:02:50 +0900

    chore(app): Application 클래스 실행 흐름 정리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import racingcar.controller.*;
+import racingcar.race.*;
+import racingcar.view.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputReader inputReader = new InputReader();
+        OutputView outputView = new OutputView();
+        CarNameParser carNameParser = new CarNameParser();
+        MovePolicy randomMovePolicy = new RandomMovePolicy();
+        RaceResult raceResult = new RaceResult();
+        InputValidator inputValidator = new InputValidator();
+
+        CarRace carRace = new CarRace(carNameParser, randomMovePolicy, outputView);
+
+        RacingGame racingGame = new RacingGame(
+                inputReader,
+                outputView,
+                carRace,
+                raceResult,
+                inputValidator
+        );
+
+        GameExecutor gameExecutor = new GameExecutor(racingGame);
+        gameExecutor.run();
     }
 }
